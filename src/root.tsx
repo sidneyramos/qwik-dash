9635bccd2affade8c5c028c53b3a38cac45f04ec
Sidney Ramos <sidneyramos95@gmail.com>
2022-11-02 00:37:00 +1100

    Added argon dashboard

--- src/root.tsx
+++ src/root.tsx
@@ -7,8 +7,6 @@ import {
 import { RouterHead } from "./components/router-head/router-head";
 import "@fontsource/inter";
 import "@fontsource/inter/700.css";
-import normalize from "./normalize.css?inline";
-import skeleton from "./skeleton.css?inline";
 
 export default component$(() => {
   /**
@@ -17,17 +15,38 @@ export default component$(() => {
    *
    * Dont remove the `<head>` and `<body>` elements.
    */
-  useStyles$(normalize);
-  useStyles$(skeleton);
 
   return (
     <QwikCity>
       <head>
         <meta charSet="utf-8" />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/assets/img/apple-icon.png"
+        />
+        <link rel="icon" type="image/png" href="/assets/img/favicon.png" />
+        <title>Argon Dashboard 2 by Creative Tim</title>
+        <link
+          href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700"
+          rel="stylesheet"
+        />
+        <link href="/assets/css/nucleo-icons.css" rel="stylesheet" />
+        <link href="/assets/css/nucleo-svg.css" rel="stylesheet" />
+        <script
+          src="https://kit.fontawesome.com/42d5adcbca.js"
+          crossOrigin="anonymous"
+        ></script>
+        <link href="/assets/css/nucleo-svg.css" rel="stylesheet" />
+        <link
+          id="pagestyle"
+          href="/assets/css/argon-dashboard.css?v=2.0.4"
+          rel="stylesheet"
+        />
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
       </head>
-      <body lang="en">
+      <body class="g-sidenav-show  bg-gray-100" lang="en">
         <RouterOutlet />
         <ServiceWorkerRegister />
       </body>
